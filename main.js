902105e08e4364f2325f2aded253655ffec612db
shubham_97 <shubham.agarwal@clix.capital>
2021-07-22 21:08:51 +0530

    Scaffolding finished

---
+++ main.js
@@ -0,0 +1,10 @@
+let hamburgerIcon = document.getElementById('hamburgerIcon');
+let menu = document.getElementById('menu');
+
+function hideMenu() {
+  menu.style.right = '-200px';
+}
+
+function showMenu() {
+  menu.style.right = '0px';
+}
